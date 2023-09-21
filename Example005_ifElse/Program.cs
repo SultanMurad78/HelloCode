5ff39f6e17a5a8580b0d13ee896cfe3b5f97d248
SultanMurad78 <143870223+SultanMurad78@users.noreply.github.com>
2023-09-21 23:50:33 +0300

    программа для приветствия с условиями

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "sultan")
+{
+    Console.WriteLine("Приветствую Вас господин Sultan!");
+}
+else
+{
+    Console.Write("Привет!, ");
+    Console.WriteLine(username);
+}
