81573d68d7bc876f1f3f91a448dd38e54579c090
SultanMurad78 <143870223+SultanMurad78@users.noreply.github.com>
2023-09-23 02:09:14 +0300

    Задача 10. нашли индекс с помощью перебора значений

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11, 21, 13, 41, 51, 61, 17, 81, 19};
+
+int n = array.Length;
+int find = 19;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+        index++;
+}
